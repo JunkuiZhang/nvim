097df6623fbaf018357c9b36f31c2c668726fa58
Junkui Zhang <364772080@qq.com>
2023-05-16 20:30:09 +0800

    update illuminate

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -5,14 +5,38 @@ local M = {
 
 function M.config()
     local illuminate = require "illuminate"
-    vim.g.Illuminate_ftblacklist = {"alpha", "NvimTree"}
+    vim.g.Illuminate_ftblacklist = { "alpha", "NvimTree" }
     vim.api.nvim_set_keymap(
         "n",
         "<a-.>",
         '<cmd>lua require"illuminate".next_reference{wrap=true}<CR>',
         { noremap = true }
     )
+    illuminate.configure({
+        providers = {
+            "lsp",
+            "treesitter",
+        },
+        delay = 100,
+        filetypes_denylist = {
+            "DoomInfo",
+            "DressingSelect",
+            "NvimTree",
+            "Outline",
+            "TelescopePrompt",
+            "Trouble",
+            "alpha",
+            "dashboard",
+            "dirvish",
+            "fugitive",
+            "help",
+            "lsgsagaoutline",
+            "neogitstatus",
+            "norg",
+            "toggleterm",
+        },
+        under_cursor = false,
+    })
 end
 
 return M
-
