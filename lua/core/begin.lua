056d279db22164c9b1a2c6e7a3341a2d7a01c008
Junkui Zhang <364772080@qq.com>
2023-05-16 18:20:10 +0800

    use ssh to access github

--- lua/core/begin.lua
+++ lua/core/begin.lua
@@ -4,7 +4,7 @@ if not vim.loop.fs_stat(lazypath) then
         "git",
         "clone",
         "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
+        "git@github.com:folke/lazy.nvim.git",
         "--branch=stable", -- latest stable release
         lazypath,
     })
@@ -17,6 +17,9 @@ require("lazy").setup({
     },
     defaults = {
         lazy = true,
+    },
+    git = {
+        utl_format = "git@github.com:%s.git",
     },
     install = {
         colorscheme = { "catppuccin" },
