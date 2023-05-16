1a37e73ddfb3c1b175932d06e6604055e7c4fdbe
Junkui Zhang <364772080@qq.com>
2023-05-16 20:20:35 +0800

    update theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,6 +6,30 @@ return {
         config = function()
             require("catppuccin").setup({
                 flavour = "latte",
+                integrations = {
+                    treesitter = true,
+                    native_lsp = {
+                        enabled = true,
+                        virtual_text = {
+                            errors = { "italic" },
+                            hints = { "italic" },
+                            warnings = { "italic" },
+                            information = { "italic" },
+                        },
+                        underlines = {
+                            errors = { "underline" },
+                            hints = { "underline" },
+                            warnings = { "underline" },
+                            information = { "underline" },
+                        },
+                    },
+                    cmp = true,
+                    illuminate = true,
+                    indent_blankline = { enabled = true, colored_indent_levels = false },
+                    mason = true,
+                    nvimtree = true,
+                    semantic_tokens = true,
+                },
             })
         end,
     },
