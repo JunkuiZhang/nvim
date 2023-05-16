dd3dce6e9788bd89d92c8743a4b500f2f20b24db
Junkui Zhang <364772080@qq.com>
2023-05-16 21:27:14 +0800

    lua lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -52,7 +52,7 @@ function M.config()
 
         server = vim.split(server, "@")[1]
 
-        local require_ok, conf_opts = pcall(require, "settings." .. server)
+        local require_ok, conf_opts = pcall(require, "langcfgs." .. server)
         if require_ok then
             Opts = vim.tbl_deep_extend("force", conf_opts, Opts)
         end
